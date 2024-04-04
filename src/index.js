4544e737b2d7ac2cb3a082a03e3c1eecedb79f3a
Sunjuhyeon <sk3488@naver.com>
2024-04-04 11:50:03 +0900

    node 디렉토리에 폴더 옮김

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* HashRouter로 라우터 인식못하는 오류 잡음(url에 #이 들어가므로 차우 다른 방법 모색해야함) */}
     <HashRouter>
       <App />
     </HashRouter>
